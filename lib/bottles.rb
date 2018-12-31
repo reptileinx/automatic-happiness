08d1939ed0f9d5051329ac981c2a5ed9860f2d9e
@reptileinx <reptileinx@gmail.com>
2019-01-01 09:03:11 +1100

    pass high test

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,4 +1,9 @@
 class Bottles
   def verse(_)
+    <<~VERSE
+      99 bottles of beer on the wall, 99 bottles of beer.
+      Take one down and pass it around, 98 bottles of beer on the wall.
+    VERSE
+
   end
 end
