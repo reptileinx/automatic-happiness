e8a38e0fca106c56967219f06d2e45f807011a3a
@reptileinx <reptileinx@gmail.com>
2019-01-01 15:55:39 +1100

    bottles can be taken from container

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -13,7 +13,7 @@ class Bottles
       VERSE
     else
       <<~VERSE
-        #{number} bottles of beer on the wall, #{number} bottles of beer.
+        #{number} #{container(number)} of beer on the wall, #{number} bottles of beer.
         Take one down and pass it around, #{number - 1} #{container(number - 1)} of beer on the wall.
       VERSE
     end
