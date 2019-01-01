9bc18ca51ce791d12a2560f59ea075dca853dd21
reptileinx <reptileinx@gmail.com>
2019-01-01 19:23:28 +1100

    add argument to pronoun for one

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -14,7 +14,7 @@ class Bottles
     else
       <<~VERSE
         #{number} #{container(number)} of beer on the wall, #{number} #{container(number)} of beer.
-        Take #{pronoun} down and pass it around, #{number - 1} #{container(number - 1)} of beer on the wall.
+        Take #{pronoun(number)} down and pass it around, #{number - 1} #{container(number - 1)} of beer on the wall.
       VERSE
     end
   end
