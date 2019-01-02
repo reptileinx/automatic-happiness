c2961e068e56e35bef9323c8631658c96816e07b
reptileinx <reptileinx@gmail.com>
2019-01-02 14:33:51 +1100

    implement conditional

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -45,6 +45,10 @@ class Bottles
       end
   end
   def action(number=0)
-    'Go to the store and buy some more'
+    if number == 0
+      'Go to the store and buy some more'
+    else
+      "Take #{pronoun(number)} down and pass it around"    
+    end
   end
 end
