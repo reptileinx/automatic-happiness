4912f53203c2f8c2b97eb9f60186518e461f0eba
reptileinx <reptileinx@gmail.com>
2019-01-03 09:11:10 +1100

    use successor on bottle number class

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -31,11 +31,11 @@ class Bottles
   end
 
   def successor(number)
-      BottleNumber.new(number).successor(number)
       if number == 0
         99
       else
         number - 1
       end
+      BottleNumber.new(number).successor(number)
   end
 end
