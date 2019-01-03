f6ac901fa91af567caeb114261b63c41f057df7e
reptileinx <reptileinx@gmail.com>
2019-01-03 19:15:23 +1100

    calling container directly from cached object

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -3,7 +3,7 @@ class Bottles
   def verse(number)
       bottle_number = BottleNumber.new(number)
     <<~VERSE
-        #{bottle_number.quantity.capitalize} #{container(number)} of beer on the wall, #{bottle_number.quantity} #{container(number)} of beer.
+        #{bottle_number.quantity.capitalize} #{container(number)} of beer on the wall, #{bottle_number.quantity} #{bottle_number.container} of beer.
         #{bottle_number.action}, #{quantity(bottle_number.successor)} #{container(bottle_number.successor)} of beer on the wall.
     VERSE
   end
