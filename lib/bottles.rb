c73366f709e62949e2b429f56ddcc5d8ebc77c0b
reptileinx <reptileinx@gmail.com>
2019-01-03 20:49:53 +1100

    creating the successor

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -2,6 +2,7 @@ require_relative '../lib/bottle_number'
 class Bottles
   def verse(number)
       bottle_number = BottleNumber.new(number)
+      next_bottle_number = BottleNumber.new(bottle_number.successor)
     <<~VERSE
         #{bottle_number.quantity.capitalize} #{container(number)} of beer on the wall, #{bottle_number.quantity} #{bottle_number.container} of beer.
         #{bottle_number.action}, #{quantity(bottle_number.successor)} #{container(bottle_number.successor)} of beer on the wall.
