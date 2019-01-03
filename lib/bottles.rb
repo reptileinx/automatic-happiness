a46f0c718ac0eaf1c83428329dcf019d0fe09b7d
reptileinx <reptileinx@gmail.com>
2019-01-03 23:44:05 +1100

    introducing next number for container

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -5,7 +5,7 @@ class Bottles
       next_bottle_number = BottleNumber.new(bottle_number.successor)
     <<~VERSE
         #{bottle_number.quantity.capitalize} #{container(number)} of beer on the wall, #{bottle_number.quantity} #{bottle_number.container} of beer.
-        #{bottle_number.action}, #{next_bottle_number.quantity} #{container(bottle_number.successor)} of beer on the wall.
+        #{bottle_number.action}, #{next_bottle_number.quantity} #{next_bottle_number.container} of beer on the wall.
     VERSE
   end
   def verses(startpoint, endpoint)
