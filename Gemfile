4c5eee09c85e3ab6f5c9c68e5d1c3b948b3444da
@reptileinx <reptileinx@gmail.com>
2019-09-13 14:04:53 +1000

    using rubocop to format

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 gem 'minitest', '~> 5.4'
 
-gem "minitest-color", "~> 0.0.2"
+gem 'minitest-color', '~> 0.0.2'
 
-gem "minitest-autotest", "~> 1.0"
+gem 'minitest-autotest', '~> 1.0'
--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,13 +1,12 @@
 require_relative '../lib/bottle_number'
 
 class Bottles
-
   def verse(number)
-      bottle_number      = BottleNumber.new(number)
-      next_bottle_number = BottleNumber.new(bottle_number.successor)
-      "#{bottle_number} of beer on the wall, ".capitalize +
-      "#{bottle_number} of beer.\n" +
-      "#{bottle_number.action}, " +
+    bottle_number = BottleNumber.new(number)
+    next_bottle_number = BottleNumber.new(bottle_number.successor)
+    "#{bottle_number} of beer on the wall, ".capitalize +
+      "#{bottle_number} of beer.\n" \
+      "#{bottle_number.action}, " \
       "#{next_bottle_number} of beer on the wall.\n"
   end
 
